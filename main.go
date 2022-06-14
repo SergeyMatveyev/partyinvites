6f5be923750117e48609f7b1b1ad9b4e8903365d
Semat <s.matveyev31@gmail.com>
2022-06-14 08:13:53 +0300

    Creaat form processing function

--- main.go
+++ main.go
@@ -43,18 +43,33 @@ func listHandler(writer http.ResponseWriter, request *http.Request) {
 	templates["list"].Execute(writer, responses)
 }
 
+//
+type formData struct {
+	*Rsvp
+	Errors []string
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		templates["form"].Execute(writer, formData{
+			Rsvp: &Rsvp{}, Errors: []string{},
+		})
+
+	}
+}
+
 // ========================================================
 func main() {
 	loadTemlates()
 
 	http.HandleFunc("/", welcomeHandler)
 	http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/form", formHandler)
 
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
-		fmt.Println((err))
+		fmt.Println(err)
 	}
-
 }
 
 // ========================================================
